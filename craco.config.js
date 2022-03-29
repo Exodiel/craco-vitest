c190d6ef5d96e232e355b186e522863cd27ec238
Jipson Medardo Saad Intriago <jsaad@Todo1of.com.ec>
2022-03-29 16:56:11 -0500

    Add craco version alpha to work with latest version of cra

---
+++ craco.config.js
@@ -0,0 +1,20 @@
+
+module.exports = {
+  webpack: {
+    configure: {
+      output: {
+        filename: 'js/main.js',
+        chunkFilename: 'js/main.[id].js?v=[hash]'
+      },
+      optimization: {
+        runtimeChunk: false,
+        splitChunks: {
+          cacheGroups: {
+            default: false
+          },
+          chunks: () => false
+        }
+      }
+    }
+  }
+}
---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React, { Fragment } from 'react';
+
+const App = () => {
+  return (
+    <Fragment>
+      <h1>Hola mundo React Craco Vitest!</h1>
+    </Fragment>
+  );
+}
+
+export default App;
