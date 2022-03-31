6438e3199ed94dd27a159f5aa156a034b52f0abb
Jipson Medardo Saad Intriago <jsaad@Todo1of.com.ec>
2022-03-31 14:04:49 -0500

    Add lint config to the project

--- craco.config.js
+++ craco.config.js
@@ -1,20 +1,19 @@
-
 module.exports = {
-  webpack: {
-    configure: {
-      output: {
-        filename: 'js/main.js',
-        chunkFilename: 'js/main.[id].js?v=[hash]'
-      },
-      optimization: {
-        runtimeChunk: false,
-        splitChunks: {
-          cacheGroups: {
-            default: false
-          },
-          chunks: () => false
-        }
-      }
-    }
-  }
-}
+	webpack: {
+		configure: {
+			output: {
+				filename: 'js/main.js',
+				chunkFilename: 'js/main.[id].js?v=[hash]',
+			},
+			optimization: {
+				runtimeChunk: false,
+				splitChunks: {
+					cacheGroups: {
+						default: false,
+					},
+					chunks: () => false,
+				},
+			},
+		},
+	},
+};
