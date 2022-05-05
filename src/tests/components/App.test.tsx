7ca97bae4a07bf8773506ceeae26c18d81934678
Jipson Medardo Saad Intriago <jsaad@Todo1of.com.ec>
2022-05-05 11:13:02 -0500

    Add typescript to the project, replace tailwind and pass to use primereact and primeflex, remove sass dependendies and just use css

---
+++ src/tests/components/App.test.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { describe, test, expect } from 'vitest'
+import { render, screen } from '@testing-library/react'
+import App from '../../components/App'
+
+describe('When render <App />', () => {
+	test('should show text content', () => {
+		render(<App />)
+		expect(
+			screen.getByText('Hello world with React And Craco And Vitest!')
+		).toBeInTheDocument()
+	})
+})
